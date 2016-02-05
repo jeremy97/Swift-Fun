ed1737735615055a92dff67cbb23c4766b07a5fc
Jeremy Quinn <jeremy.q97@gmail.com>
2016-02-04 21:56:30 -0500

    Initial Commit
    Some progress, picking up basic iOS dev

---
+++ MathOps.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var butts = 15
+
+var ayy = 5
+
+var sum = butts + ayy
+
+var multi = 3 * 15
+
+var a = 10, b = 10, c = 10
+
+let d = 2, e = 2, f = 2
+
+var dubs:Double
+
+var integer:Int = 423
+
+var sup:String = "Hello"
+
+var worldwide:String = "World"
+
+var combo = sup + worldwide
+
+var cccccombo = "\(sup)\(worldwide)"
